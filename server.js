295dba55ca3288704695fe91d0523b17c39fe735
sonderangebot10 <37372712+sonderangebot10@users.noreply.github.com>
2019-11-27 10:32:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -35,13 +35,37 @@ app.get('/api/changeLight', (req,res) => {
   let device = req.query.device;
 
   jsonData.data[room].devices[device].data.state = !jsonData.data[room].devices[device].data.state;
-  res.json({state: jsonData.data[room].devices[device].data.state});
+  res.json({state: !!jsonData.data[room].devices[device].data.state});
   console.log('Updated light');
 });
 
-// app.get('*', (req,res) =>{
-//     res.sendFile(path.join(__dirname+'/client/public/index.html'));
-// });
+app.get('/api/addRoom', (req,res) => {
+  let room_name = req.query.room_name;
+
+  jsonData.data.push({room: room_name, devices:[]});
+  res.json({state: 'success'});
+  console.log('Added a room');
+});
+
+app.get('/api/addDevice', (req,res) => {
+  let room = req.query.room;
+  let type = req.query.type;
+  let name = req.query.name;
+
+  if (type == 10){
+    jsonData.data[room].devices.push({name: name, type: 'heater', data: {temperature: '15'}});
+  }
+  else if(type == 20){
+    jsonData.data[room].devices.push({name: name, type: 'light', data: {state: false}});
+  }
+
+  res.json({state: 'success'});
+  console.log('Added a device');
+});
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/public/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
